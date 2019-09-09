36e8a1fe17c58d54a15c573a3ef9d1f74d523baf
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-09 14:41:15 -0700

    working on getting ion phaser to work with phaser example game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Play from "./components/Play";
+import Profile from "./components/Profile";
+import Scores from "./components/Scores";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+	state = {
+		userName: ""
+	}
+  
+	render() {
+  
+	  return (
+		<Router>
+
+		  <Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/profile" exact render={(props) => <Profile {...props} user={this.state} />} />
+			<Route path="/scores" render={(props) => <Scores {...props} user={this.state} />} />
+			<Route path="/play" component={Play} />
+			<Route render={() => <Redirect to="/" />} />
+		  </Switch>
+		</Router>
+	  )
+	}
+  }
 
 export default App;
