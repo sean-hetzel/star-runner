14e2b06dd4b13540957225dd98ecc06177502911
Sean Hetzel <seanhetzel1@gmail.com>
2019-09-10 18:08:36 -0700

    style home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <h1>Welcome Pilot</h1>
-                <h2>The off world colonies need your help. The replicants are cutting off supply lines. 
-                    Your mission is to deliver critical medical supplies to Arcadia 234 and to eliminate any hostile replicants along the way. </h2><br></br>
-                <h1>Controls:</h1>
-                <h2></h2>
+                <h1>WELCOME PILOT</h1>
+                <h2>> The off world colonies need your help. The replicants are cutting off supply lines. 
+                    Your mission is to deliver critical medical supplies to Arcadia 234 and to eliminate any hostile replicants along the way. >_</h2><br></br>
+                    
+                <h1>CONTROLS:</h1>
+                <h2>FLY UP ▲ | FLY DOWN ▼ | FLY BACK ◄ | FLY FORWARD ► | SHOOT ⎵</h2><br></br>
+                <h1>Good luck out there. You'll need it.</h1><br></br>
                 <Link className="brk-btn" to="/play">
                     Fly
                 </Link>
