0f41a90b04a157505b41760e15a1f79f8985c689
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-11 14:09:49 -0700

    teaked controls n stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,15 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <h2 className="home_info">STAR DATE: JULY 2, 2154</h2>
-                <h2 className="home_info">LOGS: PRE-FLIGHT BREIF</h2>
-                <h2 className="home_info">STATUS: URGENT</h2>
-                <h2 className="home_info">>_</h2>
+                <h4 className="home_info">STAR DATE: JULY 2, 2154</h4>
+                <h4 className="home_info">LOGS: PRE-FLIGHT BREIF</h4>
+                <h4 className="home_info">STATUS: URGENT</h4>
+                <h4 className="home_info">>_</h4>
                 <br></br>
                 <h1>WELCOME PILOT</h1>
                 <h2>
                     The off world colonies need your help. The replicants are
-                    cutting off supply lines and revolting. Your mission is to
+                    cutting off supply lines and endangering millions. Your mission is to
                     deliver critical releif supplies to Arcadia 234 and
                     eliminate any hostile replicants along the way.{" "}
                 </h2>
