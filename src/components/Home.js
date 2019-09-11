491d918042f3c294c0ab1e93e2a002c5ef609f34
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-10 17:50:03 -0700

    styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-
-    render(){
-        return(
-            <h1>Home</h1>
-        )
+    render() {
+        return (
+            <>
+                <h1>Welcome Pilot</h1>
+                <h2>The off world colonies need your help. The replicants are cutting off supply lines. 
+                    Your mission is to deliver critical medical supplies to Arcadia 234 and to eliminate any hostile replicants along the way. </h2><br></br>
+                <h1>Controls:</h1>
+                <h2></h2>
+                <Link className="brk-btn" to="/play">
+                    Fly
+                </Link>
+            </>
+        );
     }
 }
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,28 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav mr-auto">
-              <Link className="brk-btn" to="/">
-                Home
-              </Link>
-              <Link className="brk-btn" to="/play">
-                Play
-              </Link>
-              <Link className="brk-btn" to="/scores">
-                Stats
-              </Link>
-              <Link className="brk-btn" to="/profile">
-                Profile
-              </Link>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav id="nav_bar">
+                <Link id="star_runner_logo" to="/">
+                    STAR RUNNER
+                </Link>
+                <Link className="brk-btn" to="/">
+                    Home
+                </Link>
+                <Link className="brk-btn" to="/play">
+                    Play
+                </Link>
+                <Link className="brk-btn" to="/scores">
+                    Stats
+                </Link>
+                <Link className="brk-btn" to="/profile">
+                    Profile
+                </Link>
+            </nav>
+        );
+    }
 }
 
 export default NavBar;
