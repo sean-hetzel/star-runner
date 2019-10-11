8bc60a15ded0fceaf1d64fd5b2917cf199d30a7b
Seanhetzel <seanhetzel1@gmail.com>
2019-10-10 21:43:56 -0700

    fixed font path

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,10 +23,10 @@ class NavBar extends React.Component {
                 <iframe
                     id="soundcloud"
                     width="300"
-                    height="95"
+                    height="105"
                     scrolling="no"
                     frameBorder="no"
-                    allow="no"
+                    allow="autoplay"
                     src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/862763829&color=%23000000&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
                 ></iframe>
                 <div id="red_line_top"></div>
