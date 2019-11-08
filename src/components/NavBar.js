219f445105605ff6f66e6e0d0f32e41f79bfb42b
Seanhetzel <seanhetzel1@gmail.com>
2019-11-07 16:44:03 -0800

    added favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,7 @@ class NavBar extends React.Component {
                     height="105"
                     scrolling="no"
                     frameBorder="no"
-                    allow="autoplay"
+                    allow="no" // autoplay
                     src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/862763829&color=%23000000&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
                 ></iframe>
                 <div id="red_line_top"></div>
