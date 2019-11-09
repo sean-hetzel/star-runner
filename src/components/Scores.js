415c33c9ff8f923b66abeacce1e7080abca53f1f
Seanhetzel <seanhetzel1@gmail.com>
2019-11-08 18:25:47 -0800

    finished rank page

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 
 class Scores extends React.Component {
+
+    renderTableData(i) {
+        return this.props.scores.map(score => {
+            const { id, display_name, high_score } = score;
+            return (
+                <tr key={id}>
+                    <td className="rank">{i}</td>
+                    <td>{display_name}</td>
+                    <td>{high_score.toLocaleString()}</td>
+                    {i++}
+                </tr>
+            );
+        });
+    }
+
     render() {
+        let i = 1;
         return (
             <>
-                <h1>HIGH SCORES:</h1>;
-                {this.props.scores.map(user => {
-                    return <> <h1>{user.display_name} {user.high_score}</h1></>;
-                })}
-                
+                <h1>HIGH SCORES</h1>;
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>RANK</th>
+                            <th>PILOT</th>
+                            <th>HIGHEST SCORE</th>
+                        </tr>
+                        {this.renderTableData(i)}
+                    </tbody>
+                </table>
             </>
         );
     }
