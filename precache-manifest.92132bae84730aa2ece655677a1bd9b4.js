8be873839bfa8b6c5eb75c9e7ecd0617adfc7361
Seanhetzel <seanhetzel1@gmail.com>
2019-11-18 12:09:25 -0800

    Updates

---
+++ precache-manifest.92132bae84730aa2ece655677a1bd9b4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d33a01271572a9a7c3f0415329bc07",
+    "url": "/star-runner/index.html"
+  },
+  {
+    "revision": "a09d9b7899432ea1eddc",
+    "url": "/star-runner/static/css/main.34de441d.chunk.css"
+  },
+  {
+    "revision": "932bbca04e6969725ab4",
+    "url": "/star-runner/static/js/2.202f3e9f.chunk.js"
+  },
+  {
+    "revision": "5b9283f732dac0824d69",
+    "url": "/star-runner/static/js/3.eeb2175d.chunk.js"
+  },
+  {
+    "revision": "bc5b5d34ce7181b09de1",
+    "url": "/star-runner/static/js/4.f3828da0.chunk.js"
+  },
+  {
+    "revision": "f946b7e3475e9a078445",
+    "url": "/star-runner/static/js/5.d4a59185.chunk.js"
+  },
+  {
+    "revision": "700d8f0ef31fa177c5dc",
+    "url": "/star-runner/static/js/6.9719f22b.chunk.js"
+  },
+  {
+    "revision": "a4cec34bff7133e5d195",
+    "url": "/star-runner/static/js/7.7f8f58c8.chunk.js"
+  },
+  {
+    "revision": "d65d437284719214fefa",
+    "url": "/star-runner/static/js/8.8adea2b9.chunk.js"
+  },
+  {
+    "revision": "a09d9b7899432ea1eddc",
+    "url": "/star-runner/static/js/main.ac7a1fa5.chunk.js"
+  },
+  {
+    "revision": "4292bb6a4a31c16af051",
+    "url": "/star-runner/static/js/runtime~main.b7d1dd6d.js"
+  },
+  {
+    "revision": "6db268c1fc064e939d62976faa9cd356",
+    "url": "/star-runner/static/media/Alien-Encounters-Italic.6db268c1.ttf"
+  },
+  {
+    "revision": "3eae963b6dcfad98790b3cadac17e5e6",
+    "url": "/star-runner/static/media/asteroid.3eae963b.png"
+  },
+  {
+    "revision": "431b99d2aa2576def6aa2b940c6fff1f",
+    "url": "/star-runner/static/media/blue.431b99d2.png"
+  },
+  {
+    "revision": "f5f8c8eb2374295a6ebcff110900847d",
+    "url": "/star-runner/static/media/crash.f5f8c8eb.wav"
+  },
+  {
+    "revision": "fddd4e2e1afeff699fa15c1e2d6518d0",
+    "url": "/star-runner/static/media/github-logo-red.fddd4e2e.png"
+  },
+  {
+    "revision": "24184c8218978863be1c43b20754493c",
+    "url": "/star-runner/static/media/machineGun.24184c82.wav"
+  },
+  {
+    "revision": "ab06dbcb9cc4cbac031d4068f9fe43d9",
+    "url": "/star-runner/static/media/rocket.ab06dbcb.wav"
+  },
+  {
+    "revision": "40c188330d91a661b0bd49c762d819f3",
+    "url": "/star-runner/static/media/space-station-sprite-sheet.40c18833.png"
+  },
+  {
+    "revision": "11752848c21ef3f9545d286981bb9648",
+    "url": "/star-runner/static/media/yellow.11752848.png"
+  }
+]);
