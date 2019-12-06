e99f77630f3e079e99f6b57aa82e5c579c6fa53b
Seanhetzel <seanhetzel1@gmail.com>
2019-12-06 00:29:09 -0800

    Updates

---
+++ precache-manifest.e7c1ff27fa36635b19e47ab1bb6d826d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d96a40fb75991a426dbf00eec72789",
+    "url": "/star-runner/index.html"
+  },
+  {
+    "revision": "0a78f0d8b544d12da0d0",
+    "url": "/star-runner/static/css/main.afea5b0e.chunk.css"
+  },
+  {
+    "revision": "d9371f65ed5c434fa2e5",
+    "url": "/star-runner/static/js/2.ca51e3f3.chunk.js"
+  },
+  {
+    "revision": "76960ef5567b122d706b",
+    "url": "/star-runner/static/js/3.c12a177a.chunk.js"
+  },
+  {
+    "revision": "c60b7a6a038ec7d5809d",
+    "url": "/star-runner/static/js/4.f9349670.chunk.js"
+  },
+  {
+    "revision": "ce2243bc4dda151812ba",
+    "url": "/star-runner/static/js/5.6fdf88e7.chunk.js"
+  },
+  {
+    "revision": "0bc2d4ea94b1862177d0",
+    "url": "/star-runner/static/js/6.4a94c6a6.chunk.js"
+  },
+  {
+    "revision": "f273a4303ba5822995e1",
+    "url": "/star-runner/static/js/7.c10d39e8.chunk.js"
+  },
+  {
+    "revision": "a7f5f067effd312f88a1",
+    "url": "/star-runner/static/js/8.bc76608d.chunk.js"
+  },
+  {
+    "revision": "0a78f0d8b544d12da0d0",
+    "url": "/star-runner/static/js/main.1f0dbbf5.chunk.js"
+  },
+  {
+    "revision": "8406c7f93e01196341f0",
+    "url": "/star-runner/static/js/runtime~main.7b8f327b.js"
+  },
+  {
+    "revision": "6db268c1fc064e939d62976faa9cd356",
+    "url": "/star-runner/static/media/Alien-Encounters-Italic.6db268c1.ttf"
+  },
+  {
+    "revision": "293d4f28a4f803b7aaf2bea61bc9c3de",
+    "url": "/star-runner/static/media/arcadia-234.293d4f28.png"
+  },
+  {
+    "revision": "6b3a1bff96ff950ccad7457d1e1a25aa",
+    "url": "/star-runner/static/media/arcadia-station-interior.6b3a1bff.png"
+  },
+  {
+    "revision": "3810507572877dd9b578888849beb4c3",
+    "url": "/star-runner/static/media/arcadia-station-sprite-sheet.38105075.png"
+  },
+  {
+    "revision": "3eae963b6dcfad98790b3cadac17e5e6",
+    "url": "/star-runner/static/media/asteroid.3eae963b.png"
+  },
+  {
+    "revision": "431b99d2aa2576def6aa2b940c6fff1f",
+    "url": "/star-runner/static/media/blue.431b99d2.png"
+  },
+  {
+    "revision": "6fd30c288f2d47da7de5a20807ad2534",
+    "url": "/star-runner/static/media/crash.6fd30c28.mp3"
+  },
+  {
+    "revision": "fddd4e2e1afeff699fa15c1e2d6518d0",
+    "url": "/star-runner/static/media/github-logo-red.fddd4e2e.png"
+  },
+  {
+    "revision": "fba7327385db3fab63e6043ca39aa436",
+    "url": "/star-runner/static/media/gun.fba73273.mp3"
+  },
+  {
+    "revision": "4943b72e580ca1b7717a57e2096d71b0",
+    "url": "/star-runner/static/media/mars-station-sprite-sheet.4943b72e.png"
+  },
+  {
+    "revision": "caf029d2c1425e88a1a8ac8b8e499e4f",
+    "url": "/star-runner/static/media/mars.caf029d2.png"
+  },
+  {
+    "revision": "8870999c61ac5afbeed7d2572e1b2763",
+    "url": "/star-runner/static/media/rocket.8870999c.mp3"
+  },
+  {
+    "revision": "4f6e485a96118ba211e61bbde417e531",
+    "url": "/star-runner/static/media/saturn.4f6e485a.png"
+  },
+  {
+    "revision": "11752848c21ef3f9545d286981bb9648",
+    "url": "/star-runner/static/media/yellow.11752848.png"
+  }
+]);
