9c06e687062bfa039d9a2a3e0804ee58195cd5fa
Seanhetzel <seanhetzel1@gmail.com>
2019-12-14 22:48:44 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-runner/precache-manifest.e05c1af203daad4322187d2921314d2d.js"
+  "/star-runner/precache-manifest.937b943989c8b53a6d97e885dc87eef6.js"
 );
 
 self.addEventListener('message', (event) => {
