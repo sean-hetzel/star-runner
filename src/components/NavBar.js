e709262125b1b3498b7d94f69c1329a521aee27c
Seanhetzel <seanhetzel1@gmail.com>
2020-01-05 14:17:40 -0800

    added track name and artist to player and turned autoplay on

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -61,7 +61,7 @@ class NavBar extends React.Component {
           width={100}
           btnColor="red"
           progressColor="red"
-          autoplay={false}
+          autoplay={true}
         />
 
         <a
