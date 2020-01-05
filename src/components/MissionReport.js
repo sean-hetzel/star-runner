6498aaaa4f96d4cc97c078f3cf7c3cd1048d4809
Seanhetzel <seanhetzel1@gmail.com>
2020-01-04 18:36:16 -0800

    score is being sent to backend through MissionReport  after game over

--- src/components/MissionReport.js
+++ src/components/MissionReport.js
@@ -3,30 +3,36 @@ import { EventEmitter } from "../events.js";
 
 const API = "https://agile-atoll-75530.herokuapp.com/api/v1/scores";
 
-function reportScore(score) {
-  console.log("report score", score);
-  return score;
+class MissionReport extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0
+    };
+  }
+
+  render() {
+    EventEmitter.subscribe("updateScore", score =>
+      this.setState({ score: score })
+    );
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        display_name: "K",
+        high_score: this.state.score
+      })
+    });
+    return (
+      <div id="mission_report">
+        <h1 className="report-text">Report</h1>
+        <p>{this.state.score}</p>
+      </div>
+    );
+  }
 }
 
-export default function MissionReport() {
-  EventEmitter.subscribe("updateScore", score => reportScore(score));
-
-  fetch(API, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      display_name: "Mariette",
-      high_score: reportScore()
-    })
-  });
-
-  return (
-    <div id="mission_report">
-      <h1 className="report-text">Report</h1>
-      <p>{reportScore()}</p>
-    </div>
-  );
-}
+export default MissionReport;
